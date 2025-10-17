669d2aeca2e089bc49bc54da782d2f53bf04984d
Hawk Sky <synonlysavezone@gmail.com>
2025-10-17 16:37:29 +0700

    Demo AQI

---
+++ config.py
@@ -0,0 +1,13 @@
+API_KEY = '64e4752250846f70bbe89e0660f45c09d90b3cb7'  # Đăng ký tại aqicn.org/api
+PROVINCES_LIST = [
+    'hanoi', 
+    'ho-chi-minh-city', 
+    'danang'
+]  # Mở rộng sau (slug API)
+PROVINCE_MAPPING = {
+    'hanoi': 'Ha Noi', 
+    'ho-chi-minh-city': 'Ho Chi Minh City', 
+    'danang': 'Da Nang'
+}
+DATA_PATH = 'data/processed/vn_pollution.geojson'
+DB_PATH = 'data/processed/aqi_history.db'
